e10dc644d9bfd73466806dbdcdd065193d11c4ed
Huang jr <jr@nlplab.cc>
2016-01-24 17:14:43 +0800

    easy version, do not need to give the old_file_path

--- amy/change_path.py
+++ amy/change_path.py
@@ -2,10 +2,11 @@ import os
 import sys
 import fileinput
 
-for filename in os.listdir(r'/Users/jr/Downloads/amy'):
+#FILE_PATH: current working directory
+for filename in os.listdir(r'FILE_PATH'):
 	if filename.split('.')[1] == 'xml':
 		for line in fileinput.input(filename, inplace = 1):
 			if 'FileName' in line:
-				line = '<SinglePage FileName="/Users/jr/Downloads/amy/' + line.split('/')[-1]
+				line = '<SinglePage FileName="FILE_PATH' + line.split('/')[-1]
 			sys.stdout.write(line)
 				
